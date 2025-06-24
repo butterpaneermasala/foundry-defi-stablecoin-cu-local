cfdf8f00482b383f4b7e482d876cffa1fc92bee8
Satya <satyapradhan1732@gmail.com>
2025-06-24 15:25:36 +0530

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+Stable coin
+
+1. Realtive Stability: Anchpred or pegged -> $1.00
+    1. Chainlink price feed.
+    2. Set a function to exchange ETH & BTC -> $$$
+2. Stablility Mechanism (Minting): Algorithmic (Decentralized)
+    1. People can only mint tge stablecoin with enough collateral (coded)
+3. Collateral: exogenous (Cyrpto)
+    1. wRTH
+    2. wBTC
---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity  ^0.8.19;
+
+/*
+* @tile DecentralizedSatbleCoin
+* @author Satya pradhan
+* Collateral: Exogeneous (ETH & BTC)
+* Mintiing Stability: pegged to USD
+*
+*
+*/
